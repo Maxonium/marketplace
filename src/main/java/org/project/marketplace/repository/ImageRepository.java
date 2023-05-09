a113fb24c36f3347921c2f29a986a231895cbc1a
Maxim <maksimchugunov8@gmail.com>
2023-05-09 15:22:23 +0300

    Created repo "image"

---
+++ src/main/java/org/project/marketplace/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package org.project.marketplace.repository;
+
+import org.project.marketplace.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long>
+{
+
+}
