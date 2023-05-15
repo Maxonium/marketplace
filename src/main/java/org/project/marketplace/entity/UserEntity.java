449228032be0a7976f042b112bde48b11cf2a82d
Maxim <maksimchugunov8@gmail.com>
2023-05-15 18:02:03 +0300

    Created user entity

---
+++ src/main/java/org/project/marketplace/entity/UserEntity.java
@@ -0,0 +1,49 @@
+package org.project.marketplace.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.project.marketplace.enums.Role;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "email", unique = true)
+    private String email;
+    // Добавить телефон?
+    @Column(name = "userName")
+    private String userName;
+    @Column(name = "password", length = 1000)
+    private String password;
+    @Column(name = "userActive")
+    private boolean userActive;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "avatar_id")
+    private Image avatar;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    Set<Role> roles = new HashSet<>();
+    private LocalDateTime dateOfCreate;
+
+    @PrePersist
+    public void init()
+    {
+        dateOfCreate = LocalDateTime.now();
+    }
+}
